4fa6a2b0492d3e7b3c1aace3e32bcb0968142219
minimaledit <user7.malorita@yandex.by>
2022-09-24 14:02:36 +0300

    Cto-to sdelal

--- Program.java
+++ Program.java
@@ -1,6 +1,41 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class Program {
 
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
+        Scanner sin = new Scanner(System.in);
+        String fileName = sin.nextLine();
+        LinkedList<String> inputText = new LinkedList<>();
+        try {
+            Scanner in = new Scanner(new File(fileName));
+            while (in.hasNext())
+                inputText.add(in.nextLine());
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        for(int i = 0; i<inputText.size(); i++) {
+            String value = inputText.get(i);
+            System.out.println(value);
+        }
+        try(FileWriter writer = new FileWriter("answer.out", false)) {
+            for (int i = 0; i < inputText.size(); i++) {
+                String value = inputText.get(i);
+                try {
+                    writer.write(value + '\n');
+                    writer.flush();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+        } catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
     }
 }
